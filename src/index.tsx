3c2eaf871a09ccd27da011992d7be76dc4caae1e
Sahan Amarsha <sahanamarsha@gmail.com>
2023-01-12 11:18:19 +0530

    Update for production auth

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,29 @@ const root = createRoot(rootElement!);
 import { Amplify } from 'aws-amplify';
 import awsExports from './aws-exports';
 
-Amplify.configure(awsExports);
+const isLocalhost = Boolean(
+    window.location.hostname === "localhost" ||
+    // [::1] is the IPv6 localhost address.
+    window.location.hostname === "[::1]" ||
+    // 127.0.0.1/8 is considered localhost for IPv4.
+    window.location.hostname.match(
+        /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+    )
+);
+
+const localRedirect = "http://localhost:3000/";
+const productionRedirect = "https://master.d29thsukvcvws1.amp/";
+
+const updatedAwsConfig = {
+    ...awsExports,
+    oauth: {
+        ...awsExports.oauth,
+        redirectSignIn: isLocalhost ? localRedirect : productionRedirect,
+        redirectSignOut: isLocalhost ? localRedirect : productionRedirect,
+    }
+}
+
+Amplify.configure(updatedAwsConfig);
 root.render(
   <ThemeProvider theme={theme}>
     <AuthProvider>
